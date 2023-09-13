1e2df19d96d154f6a01d1bc6a1ec98dac3e62270
Sakshi Rahangdale <rahangdalesakshi2@gmail.com>
2023-09-13 20:29:23 +0530

    feat: navbar change

---
+++ Script/navbar.js
@@ -0,0 +1,9 @@
+// navbar.js
+
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.items');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navLinks.classList.toggle('active');
+});
