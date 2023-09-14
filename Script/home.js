439171d794dcd2b29f94a8a121fec2a62deb6a46
Sakshi Rahangdale <rahangdalesakshi2@gmail.com>
2023-09-14 12:45:39 +0530

    feat: about us modify

--- Script/home.js
+++ Script/home.js
@@ -8,4 +8,33 @@ hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
 
+//user name js
+document.addEventListener("DOMContentLoaded", function () {
+    const userName = document.getElementById("user-name");
+    const logoutButton = document.getElementById("logout");
+
+    // Check if the user is logged in by checking local storage.
+    const loggedInUser = localStorage.getItem("loggedInUser");
+
+    if (loggedInUser) {
+        // Display the user's name on the home page.
+        userName.textContent = loggedInUser;
+        
+    } else {
+        // If the user is not logged in, redirect to the login page.
+        window.location.href = "./../Pages/login.html";
+    }
+
+    // Handle logout
+    logoutButton.addEventListener("click", function () {
+        // Clear the user's data from local storage.
+        localStorage.removeItem("loggedInUser");
+        localStorage.removeItem("userPassword");
+
+        // Redirect to the login page.
+        window.location.href = "./../Pages/login.html";
+    });
+});
+
+
 //"https://eduonixjsproject.netlify.app/"
