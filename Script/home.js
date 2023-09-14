52f3aa4b46c1b0eba0a4ee3212907a6ad3c98308
Sakshi Rahangdale <rahangdalesakshi2@gmail.com>
2023-09-14 12:57:36 +0530

    feat: user profile adde on oredrs or menu page

--- Script/home.js
+++ Script/home.js
@@ -11,7 +11,6 @@ hamburger.addEventListener('click', () => {
 //user name js
 document.addEventListener("DOMContentLoaded", function () {
     const userName = document.getElementById("user-name");
-    const logoutButton = document.getElementById("logout");
 
     // Check if the user is logged in by checking local storage.
     const loggedInUser = localStorage.getItem("loggedInUser");
@@ -20,20 +19,9 @@ document.addEventListener("DOMContentLoaded", function () {
         // Display the user's name on the home page.
         userName.textContent = loggedInUser;
         
-    } else {
-        // If the user is not logged in, redirect to the login page.
-        window.location.href = "./../Pages/login.html";
-    }
+    } 
 
-    // Handle logout
-    logoutButton.addEventListener("click", function () {
-        // Clear the user's data from local storage.
-        localStorage.removeItem("loggedInUser");
-        localStorage.removeItem("userPassword");
-
-        // Redirect to the login page.
-        window.location.href = "./../Pages/login.html";
-    });
+    
 });
 
 
